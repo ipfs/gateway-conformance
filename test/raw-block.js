ca8575fc99303888c16f104ba3097f078b04095d
galargh <piotr.galar@gmail.com>
2023-02-06 10:30:27 +0100

    Add cache-control checks

--- test/raw-block.js
+++ test/raw-block.js
@@ -1,6 +1,7 @@
 import { Fixture } from '../util/fixtures.js';
 import { config } from './config.js';
 import { run } from 'declarative-e2e-test';
+import { ok } from 'assert'
 
 const IPLD_RAW_TYPE = 'application/vnd.ipld.raw';
 
@@ -47,6 +48,18 @@ const test = {
             'x-ipfs-path': `/ipfs/${Fixture.get('dir').getRootCID()}/dir/ascii.txt`,
             'x-ipfs-roots': new RegExp(Fixture.get('dir').getRootCID())
           }
+        }, function(response) {
+          const cachePragmata = (response.headers['cache-control'] || '').split(/\s*,\s*/);
+          Object.entries({
+            'public pragma': (str) => str.toLowerCase() === 'public',
+            'immutable pragma': (str) => str.toLowerCase() === 'immutable',
+            'max-age pragma': (str) => {
+              if (!/^max-age=/i.test(str)) return false;
+              if (parseInt(str.replace('max-age=', ''), 10) < 29030400) return false; // at least 29030400
+              return true;
+            },
+          })
+          .forEach(([label, func]) => ok(cachePragmata.find(func), label))
         }]
       }
     }
