4619446a6ea69b9a8145bce41493dd486a428d4f
Laurent Senta <laurent@singulargarden.com>
2023-04-03 15:30:45 +0200

    fix: skip tar tests

--- tests/t0122_gateway_tar_test.go
+++ tests/t0122_gateway_tar_test.go
@@ -2,12 +2,8 @@ package tests
 
 import (
 	"testing"
-
-	"github.com/ipfs/gateway-conformance/tooling/test"
 )
 
 func TestGatewayTar(t *testing.T) {
-	tests := []test.CTest{}
-
-	test.Run(t, tests)
+	t.Skip()
 }
