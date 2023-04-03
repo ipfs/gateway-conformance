4977e8621172125b50ac5b4b584e0e6a8bfd33ea
galargh <piotr.galar@gmail.com>
2023-04-03 17:02:14 +0200

    chore: group unmigrated test cases

--- tests/t0116_gateway_cache_test.go
+++ tests/t0116_gateway_cache_test.go
@@ -9,107 +9,90 @@ import (
 )
 
 /* TODO
-# GET /ipns/
-    # unixfs
-    test_expect_success "GET for /ipns/ unixfs dir listing succeeds" '
-    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/" >/dev/null 2>curl_ipns_dir_listing_output
-    '
-    test_expect_success "GET for /ipns/ unixfs dir with index.html succeeds" '
-    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/" >/dev/null 2>curl_ipns_dir_index.html_output
-    '
-    test_expect_success "GET for /ipns/ unixfs file succeeds" '
-    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_ipns_file_output
-    '
-    # unixfs dir as dag-json
-    test_expect_success "GET for /ipns/ unixfs dir as DAG-JSON succeeds" '
-    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=dag-json" >/dev/null 2>curl_ipns_dir_dag-json_output &&
-    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=json" >/dev/null 2>curl_ipns_dir_json_output
-    '
-# Cache-Control: mutable /ipns/ file
-    test_expect_success "GET /ipns/ unixfs file has no Cache-Control" '
-    test_should_not_contain "< Cache-Control" curl_ipns_file_output
-    '
-# Cache-Control: mutable /ipns/dir/ (generated listing)
-    test_expect_success "GET /ipns/ unixfs dir listing has no Cache-Control" '
-    test_should_not_contain "< Cache-Control" curl_ipns_dir_listing_output
-    '
-# Cache-Control: mutable /ipns/dir/ (index.html)
-    test_expect_success "GET /ipns/ unixfs dir with index.html has no Cache-Control" '
-    test_should_not_contain "< Cache-Control" curl_ipns_dir_index.html_output
-    '
-# Cache-Control: mutable /ipns/dir/ as dag-json
-    test_expect_success "GET /ipns/ unixfs dir as dag-json has no Cache-Control" '
-    test_should_not_contain "< Cache-Control" curl_ipns_dir_dag-json_output
-    '
-# Cache-Control: mutable /ipns/dir/ as json
-    test_expect_success "GET /ipns/ unixfs dir as json has no Cache-Control" '
-    test_should_not_contain "< Cache-Control" curl_ipns_dir_json_output
-    '
-# X-Ipfs-Path
-    ## dir generated listing
-    test_expect_success "GET /ipns/ dir listing response has original content path in X-Ipfs-Path" '
-    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3" curl_ipns_dir_listing_output
-    '
-
-    ## dir static index.html
-    test_expect_success "GET /ipns/ dir index.html response has original content path in X-Ipfs-Path" '
-    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/" curl_ipns_dir_index.html_output
-    '
-
-    # file
-    test_expect_success "GET /ipns/ file response has original content path in X-Ipfs-Path" '
-    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" curl_ipns_file_output
-    '
-# X-Ipfs-Roots
-
-    ## dir generated listing
-    test_expect_success "GET /ipns/ dir listing response has logical CID roots in X-Ipfs-Roots" '
-    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID}" curl_ipns_dir_listing_output
-    '
-
-    ## dir static index.html
-    test_expect_success "GET /ipns/ dir index.html response has logical CID roots in X-Ipfs-Roots" '
-    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID}" curl_ipns_dir_index.html_output
-    '
-
-    ## file
-    test_expect_success "GET /ipns/ file response has logical CID roots in X-Ipfs-Roots" '
-    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID},${FILE_CID}" curl_ipns_file_output
-    '
-# Etag
-
-    ## dir generated listing
-    test_expect_success "GET /ipns/ dir response has special Etag for generated dir listing" '
-    test_should_contain "< Etag: \"DirIndex" curl_ipns_dir_listing_output &&
-    grep -E "< Etag: \"DirIndex-.+_CID-${ROOT3_CID}\"" curl_ipns_dir_listing_output
-    '
-
-    ## dir static index.html should use CID of  the index.html file for improved HTTP caching
-    test_expect_success "GET /ipns/ dir index.html response has dir CID as Etag" '
-    test_should_contain "< Etag: \"${ROOT4_CID}\"" curl_ipns_dir_index.html_output
-    '
-
-    ## file
-    test_expect_success "GET /ipns/ response has CID as Etag for a file" '
-    test_should_contain "< Etag: \"${FILE_CID}\"" curl_ipns_file_output
-    '
-
-		test_expect_success "GET for /ipns/ file with matching Etag in If-None-Match returns 304 Not Modified" '
-    curl -svX GET -H "If-None-Match: \"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
-    test_should_contain "304 Not Modified" curl_output
-    '
-
-		# DirIndex etag is based on xxhash(./assets/dir-index-html), so we need to fetch it dynamically
-    test_expect_success "GET for /ipfs/ dir listing with matching strong Etag in If-None-Match returns 304 Not Modified" '
-    curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
-    curl -svX GET -H "If-None-Match: \"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
-    test_should_contain "304 Not Modified" curl_output
-    '
-    test_expect_success "GET for /ipfs/ dir listing with matching weak Etag in If-None-Match returns 304 Not Modified" '
-    curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
-    curl -svX GET -H "If-None-Match: W/\"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
-    test_should_contain "304 Not Modified" curl_output
-    '
+1.
+test_expect_success "GET for /ipns/ unixfs dir listing succeeds" '
+curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/" >/dev/null 2>curl_ipns_dir_listing_output
+'
+test_expect_success "GET /ipns/ unixfs dir listing has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_dir_listing_output
+'
+test_expect_success "GET /ipns/ unixfs dir listing has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_dir_listing_output
+'
+test_expect_success "GET /ipns/ dir listing response has original content path in X-Ipfs-Path" '
+test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3" curl_ipns_dir_listing_output
+'
+test_expect_success "GET /ipns/ dir listing response has logical CID roots in X-Ipfs-Roots" '
+test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID}" curl_ipns_dir_listing_output
+'
+test_expect_success "GET /ipns/ dir response has special Etag for generated dir listing" '
+test_should_contain "< Etag: \"DirIndex" curl_ipns_dir_listing_output &&
+grep -E "< Etag: \"DirIndex-.+_CID-${ROOT3_CID}\"" curl_ipns_dir_listing_output
+'
+2.
+test_expect_success "GET for /ipns/ unixfs dir with index.html succeeds" '
+curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/" >/dev/null 2>curl_ipns_dir_index.html_output
+'
+test_expect_success "GET /ipns/ unixfs dir with index.html has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_dir_index.html_output
+'
+test_expect_success "GET /ipns/ dir index.html response has original content path in X-Ipfs-Path" '
+test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/" curl_ipns_dir_index.html_output
+'
+test_expect_success "GET /ipns/ dir index.html response has logical CID roots in X-Ipfs-Roots" '
+test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID}" curl_ipns_dir_index.html_output
+'
+test_expect_success "GET /ipns/ dir index.html response has dir CID as Etag" '
+test_should_contain "< Etag: \"${ROOT4_CID}\"" curl_ipns_dir_index.html_output
+'
+3.
+test_expect_success "GET for /ipns/ unixfs file succeeds" '
+curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_ipns_file_output
+'
+test_expect_success "GET /ipns/ unixfs file has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_file_output
+'
+test_expect_success "GET /ipns/ file response has original content path in X-Ipfs-Path" '
+test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" curl_ipns_file_output
+'
+test_expect_success "GET /ipns/ file response has logical CID roots in X-Ipfs-Roots" '
+test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID},${FILE_CID}" curl_ipns_file_output
+'
+test_expect_success "GET /ipns/ response has CID as Etag for a file" '
+test_should_contain "< Etag: \"${FILE_CID}\"" curl_ipns_file_output
+'
+4.
+test_expect_success "GET for /ipns/ unixfs dir as DAG-JSON succeeds" '
+curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=dag-json" >/dev/null 2>curl_ipns_dir_dag-json_output
+'
+test_expect_success "GET /ipns/ unixfs dir as dag-json has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_dir_dag-json_output
+'
+5.
+test_expect_success "GET for /ipns/ unixfs dir as DAG-JSON succeeds" '
+curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=json" >/dev/null 2>curl_ipns_dir_json_output
+'
+test_expect_success "GET /ipns/ unixfs dir as json has no Cache-Control" '
+test_should_not_contain "< Cache-Control" curl_ipns_dir_json_output
+'
+6.
+test_expect_success "GET for /ipns/ file with matching Etag in If-None-Match returns 304 Not Modified" '
+curl -svX GET -H "If-None-Match: \"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+test_should_contain "304 Not Modified" curl_output
+'
+7.
+# DirIndex etag is based on xxhash(./assets/dir-index-html), so we need to fetch it dynamically
+test_expect_success "GET for /ipfs/ dir listing with matching strong Etag in If-None-Match returns 304 Not Modified" '
+curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
+curl -svX GET -H "If-None-Match: \"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
+test_should_contain "304 Not Modified" curl_output
+'
+8.
+test_expect_success "GET for /ipfs/ dir listing with matching weak Etag in If-None-Match returns 304 Not Modified" '
+curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
+curl -svX GET -H "If-None-Match: W/\"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
+test_should_contain "304 Not Modified" curl_output
+'
 */
 func TestGatewayCache(t *testing.T) {
 	fixture := car.MustOpenUnixfsCar("t0116-gateway-cache.car")
