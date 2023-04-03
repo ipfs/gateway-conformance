685a5aaac8555ec3051d1f2f7b864370f69c8857
galargh <piotr.galar@gmail.com>
2023-04-03 16:53:37 +0200

    chore: add comments about ported test cases

--- tests/t0116_gateway_cache_test.go
+++ tests/t0116_gateway_cache_test.go
@@ -8,14 +8,131 @@ import (
 	. "github.com/ipfs/gateway-conformance/tooling/test"
 )
 
-/* These tests do not cover the following:
-** - /ipns/ paths
-** - "If-None-Match" header handling for strong ETags for dir listings (the ones with xxhash)
- */
+/* TODO
+# GET /ipns/
+    # unixfs
+    test_expect_success "GET for /ipns/ unixfs dir listing succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/" >/dev/null 2>curl_ipns_dir_listing_output
+    '
+    test_expect_success "GET for /ipns/ unixfs dir with index.html succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/" >/dev/null 2>curl_ipns_dir_index.html_output
+    '
+    test_expect_success "GET for /ipns/ unixfs file succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_ipns_file_output
+    '
+    # unixfs dir as dag-json
+    test_expect_success "GET for /ipns/ unixfs dir as DAG-JSON succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=dag-json" >/dev/null 2>curl_ipns_dir_dag-json_output &&
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/?format=json" >/dev/null 2>curl_ipns_dir_json_output
+    '
+# Cache-Control: mutable /ipns/ file
+    test_expect_success "GET /ipns/ unixfs file has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipns_file_output
+    '
+# Cache-Control: mutable /ipns/dir/ (generated listing)
+    test_expect_success "GET /ipns/ unixfs dir listing has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipns_dir_listing_output
+    '
+# Cache-Control: mutable /ipns/dir/ (index.html)
+    test_expect_success "GET /ipns/ unixfs dir with index.html has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipns_dir_index.html_output
+    '
+# Cache-Control: mutable /ipns/dir/ as dag-json
+    test_expect_success "GET /ipns/ unixfs dir as dag-json has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipns_dir_dag-json_output
+    '
+# Cache-Control: mutable /ipns/dir/ as json
+    test_expect_success "GET /ipns/ unixfs dir as json has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipns_dir_json_output
+    '
+# X-Ipfs-Path
+    ## dir generated listing
+    test_expect_success "GET /ipns/ dir listing response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3" curl_ipns_dir_listing_output
+    '
+
+    ## dir static index.html
+    test_expect_success "GET /ipns/ dir index.html response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/" curl_ipns_dir_index.html_output
+    '
+
+    # file
+    test_expect_success "GET /ipns/ file response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" curl_ipns_file_output
+    '
+# X-Ipfs-Roots
+
+    ## dir generated listing
+    test_expect_success "GET /ipns/ dir listing response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID}" curl_ipns_dir_listing_output
+    '
+
+    ## dir static index.html
+    test_expect_success "GET /ipns/ dir index.html response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID}" curl_ipns_dir_index.html_output
+    '
+
+    ## file
+    test_expect_success "GET /ipns/ file response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID},${FILE_CID}" curl_ipns_file_output
+    '
+# Etag
+
+    ## dir generated listing
+    test_expect_success "GET /ipns/ dir response has special Etag for generated dir listing" '
+    test_should_contain "< Etag: \"DirIndex" curl_ipns_dir_listing_output &&
+    grep -E "< Etag: \"DirIndex-.+_CID-${ROOT3_CID}\"" curl_ipns_dir_listing_output
+    '
+
+    ## dir static index.html should use CID of  the index.html file for improved HTTP caching
+    test_expect_success "GET /ipns/ dir index.html response has dir CID as Etag" '
+    test_should_contain "< Etag: \"${ROOT4_CID}\"" curl_ipns_dir_index.html_output
+    '
+
+    ## file
+    test_expect_success "GET /ipns/ response has CID as Etag for a file" '
+    test_should_contain "< Etag: \"${FILE_CID}\"" curl_ipns_file_output
+    '
+
+		test_expect_success "GET for /ipns/ file with matching Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: \"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipns/$TEST_IPNS_ID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+
+		# DirIndex etag is based on xxhash(./assets/dir-index-html), so we need to fetch it dynamically
+    test_expect_success "GET for /ipfs/ dir listing with matching strong Etag in If-None-Match returns 304 Not Modified" '
+    curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
+    curl -svX GET -H "If-None-Match: \"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+    test_expect_success "GET for /ipfs/ dir listing with matching weak Etag in If-None-Match returns 304 Not Modified" '
+    curl -Is "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/"| grep -i Etag | cut -f2- -d: | tr -d "[:space:]\"" > dir_index_etag &&
+    curl -svX GET -H "If-None-Match: W/\"$(<dir_index_etag)\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+*/
 func TestGatewayCache(t *testing.T) {
 	fixture := car.MustOpenUnixfsCar("t0116-gateway-cache.car")
 
 	tests := SugarTests{
+		/*
+		test_expect_success "GET for /ipfs/ unixfs dir listing succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_ipfs_dir_listing_output
+    '
+		test_expect_success "GET /ipfs/ unixfs dir listing has no Cache-Control" '
+    test_should_not_contain "< Cache-Control" curl_ipfs_dir_listing_output
+    '
+		test_expect_success "GET /ipfs/ dir listing response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipfs/$ROOT1_CID/root2/root3" curl_ipfs_dir_listing_output
+    '
+		test_expect_success "GET /ipfs/ dir listing response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID}" curl_ipfs_dir_listing_output
+    '
+		test_expect_success "GET /ipfs/ dir response has special Etag for generated dir listing" '
+    test_should_contain "< Etag: \"DirIndex" curl_ipfs_dir_listing_output &&
+    grep -E "< Etag: \"DirIndex-.+_CID-${ROOT3_CID}\"" curl_ipfs_dir_listing_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ unixfs dir listing succeeds",
 			Request: Request().
@@ -33,6 +150,23 @@ func TestGatewayCache(t *testing.T) {
 						Matches("DirIndex-.*_CID-%s", fixture.MustGetCid("root2", "root3")),
 				),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ unixfs dir with index.html succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/" >/dev/null 2>curl_ipfs_dir_index.html_output
+    '
+    test_expect_success "GET /ipfs/ unixfs dir with index.html has expected Cache-Control" '
+    test_should_contain "< Cache-Control: public, max-age=29030400, immutable" curl_ipfs_dir_index.html_output
+    '
+		test_expect_success "GET /ipfs/ dir index.html response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipfs/$ROOT1_CID/root2/root3/root4/" curl_ipfs_dir_index.html_output
+    '
+		test_expect_success "GET /ipfs/ dir index.html response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID}" curl_ipfs_dir_index.html_output
+    '
+		test_expect_success "GET /ipfs/ dir index.html response has dir CID as Etag" '
+    test_should_contain "< Etag: \"${ROOT4_CID}\"" curl_ipfs_dir_index.html_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ unixfs dir with index.html succeeds",
 			Request: Request().
@@ -50,6 +184,23 @@ func TestGatewayCache(t *testing.T) {
 						Equals("\"%s\"", fixture.MustGetCid("root2", "root3", "root4")),
 				),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ unixfs file succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_ipfs_file_output
+    '
+		test_expect_success "GET /ipfs/ unixfs file has expected Cache-Control" '
+    test_should_contain "< Cache-Control: public, max-age=29030400, immutable" curl_ipfs_file_output
+    '
+		test_expect_success "GET /ipfs/ file response has original content path in X-Ipfs-Path" '
+    test_should_contain "< X-Ipfs-Path: /ipfs/$ROOT1_CID/root2/root3/root4/index.html" curl_ipfs_file_output
+    '
+		test_expect_success "GET /ipfs/ file response has logical CID roots in X-Ipfs-Roots" '
+    test_should_contain "< X-Ipfs-Roots: ${ROOT1_CID},${ROOT2_CID},${ROOT3_CID},${ROOT4_CID},${FILE_CID}" curl_ipfs_file_output
+    '
+		test_expect_success "GET /ipfs/ response has CID as Etag for a file" '
+    test_should_contain "< Etag: \"${FILE_CID}\"" curl_ipfs_file_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ unixfs file succeeds",
 			Request: Request().
@@ -67,10 +218,19 @@ func TestGatewayCache(t *testing.T) {
 						Equals("\"%s\"", fixture.MustGetCid("root2", "root3", "root4", "index.html")),
 				),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ unixfs dir as DAG-JSON succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/?format=dag-json" >/dev/null 2>curl_ipfs_dir_dag-json_output
+    '
+    test_expect_success "GET /ipfs/ dag-json has expected Cache-Control" '
+    test_should_contain "< Cache-Control: public, max-age=29030400, immutable" curl_ipfs_dir_dag-json_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ unixfs dir as DAG-JSON succeeds",
 			Request: Request().
-				Path("ipfs/%s/root2/root3/root4/?format=dag-json", fixture.MustGetCid()),
+				Path("ipfs/%s/root2/root3/root4/", fixture.MustGetCid()).
+				Query("format", "dag-json"),
 			Response: Expect().
 				Status(200).
 				Headers(
@@ -78,10 +238,19 @@ func TestGatewayCache(t *testing.T) {
 						Equals("public, max-age=29030400, immutable"),
 				),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ unixfs dir as DAG-JSON succeeds" '
+    curl -svX GET "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/?format=json" >/dev/null 2>curl_ipfs_dir_json_output
+    '
+    test_expect_success "GET /ipfs/ unixfs dir as json has expected Cache-Control" '
+    test_should_contain "< Cache-Control: public, max-age=29030400, immutable" curl_ipfs_dir_json_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ unixfs dir as JSON succeeds",
 			Request: Request().
-				Path("ipfs/%s/root2/root3/root4/?format=json", fixture.MustGetCid()),
+				Path("ipfs/%s/root2/root3/root4/", fixture.MustGetCid()).
+				Query("format", "json"),
 			Response: Expect().
 				Status(200).
 				Headers(
@@ -89,6 +258,13 @@ func TestGatewayCache(t *testing.T) {
 						Equals("public, max-age=29030400, immutable"),
 				),
 		},
+		/*
+		test_expect_success "HEAD for /ipfs/ with only-if-cached succeeds when in local datastore" '
+    curl -sv -I -H "Cache-Control: only-if-cached" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" > curl_onlyifcached_postitive_head 2>&1 &&
+    cat curl_onlyifcached_postitive_head &&
+    grep "< HTTP/1.1 200 OK" curl_onlyifcached_postitive_head
+    '
+		*/
 		{
 			Name: "HEAD for /ipfs/ with only-if-cached succeeds when in local datastore",
 			Request: Request().
@@ -100,6 +276,13 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(200),
 		},
+		/*
+		test_expect_success "HEAD for /ipfs/ with only-if-cached fails when not in local datastore" '
+    curl -sv -I -H "Cache-Control: only-if-cached" "http://127.0.0.1:$GWAY_PORT/ipfs/$(date | ipfs add --only-hash -Q)" > curl_onlyifcached_negative_head 2>&1 &&
+    cat curl_onlyifcached_negative_head &&
+    grep "< HTTP/1.1 412 Precondition Failed" curl_onlyifcached_negative_head
+    '
+		*/
 		{
 			Name: "HEAD for /ipfs/ with only-if-cached fails when not in local datastore",
 			Request: Request().
@@ -111,6 +294,13 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(412),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ with only-if-cached succeeds when in local datastore" '
+    curl -svX GET -H "Cache-Control: only-if-cached" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_onlyifcached_postitive_out &&
+    cat curl_onlyifcached_postitive_out &&
+    grep "< HTTP/1.1 200 OK" curl_onlyifcached_postitive_out
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ with only-if-cached succeeds when in local datastore",
 			Request: Request().
@@ -121,6 +311,13 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(200),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ with only-if-cached fails when not in local datastore" '
+    curl -svX GET -H "Cache-Control: only-if-cached" "http://127.0.0.1:$GWAY_PORT/ipfs/$(date | ipfs add --only-hash -Q)" >/dev/null 2>curl_onlyifcached_negative_out &&
+    cat curl_onlyifcached_negative_out &&
+    grep "< HTTP/1.1 412 Precondition Failed" curl_onlyifcached_negative_out
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ with only-if-cached fails when not in local datastore",
 			Request: Request().
@@ -131,6 +328,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(412),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ file with matching Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: \"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ file with matching Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
@@ -141,6 +344,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(304),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ dir with index.html file with matching Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: \"$ROOT4_CID\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ dir with index.html file with matching Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
@@ -151,6 +360,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(304),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ file with matching third Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: \"fakeEtag1\", \"fakeEtag2\", \"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ file with matching third Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
@@ -161,6 +376,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(304),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ file with matching weak Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: W/\"$FILE_CID\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ file with matching weak Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
@@ -171,6 +392,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(304),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ file with wildcard Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: *" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/root4/index.html" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ file with wildcard Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
@@ -181,6 +408,12 @@ func TestGatewayCache(t *testing.T) {
 			Response: Expect().
 				Status(304),
 		},
+		/*
+		test_expect_success "GET for /ipfs/ dir listing with matching weak Etag in If-None-Match returns 304 Not Modified" '
+    curl -svX GET -H "If-None-Match: W/\"$ROOT3_CID\"" "http://127.0.0.1:$GWAY_PORT/ipfs/$ROOT1_CID/root2/root3/" >/dev/null 2>curl_output &&
+    test_should_contain "304 Not Modified" curl_output
+    '
+		*/
 		{
 			Name: "GET for /ipfs/ dir listing with matching weak Etag in If-None-Match returns 304 Not Modified",
 			Request: Request().
