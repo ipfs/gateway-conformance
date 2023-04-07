6988157764f4b8d55ead6bcc143d84c3882f2163
Laurent Senta <laurent@singulargarden.com>
2023-04-07 11:19:43 +0200

    feat: setup ci ipns map

--- kubo-config.example.sh
+++ kubo-config.example.sh
@@ -10,3 +10,8 @@ ipfs config --json Gateway.PublicGateways '{
 		"Paths": ["/ipfs", "/ipns", "/api"]		
 	}											
 }'
+
+IPFS_NS_MAP=$(cat ./dnslinks.json | jq -r 'to_entries | map("\(.key).example.com:\(.value)") | join(",")')
+
+echo "Set the following IPFS_NS_MAP before starting the kubo daemon:"
+echo "IPFS_NS_MAP=${IPFS_NS_MAP}"
