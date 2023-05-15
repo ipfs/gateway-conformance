a3437dd484cb13e4d81dcfa24aded0c669e4f40b
Eric Guan <guanzo91@gmail.com>
2023-05-15 13:32:47 -0700

    add car-scope tests

--- tests/t0118_gateway_car_test.go
+++ tests/t0118_gateway_car_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestGatewayCar(t *testing.T) {
 	fixture := car.MustOpenUnixfsCar("t0118-test-dag.car")
+	hamtFixture := car.MustOpenUnixfsCar("t0118-ipip-402-scope.car")
 
 	tests := SugarTests{
 		{
@@ -42,13 +43,106 @@ func TestGatewayCar(t *testing.T) {
 					Header("Accept-Ranges").
 						Hint("CAR is streamed, gateway may not have the entire thing, unable to support range-requests. Partial downloads and resumes should be handled using IPLD selectors: https://github.com/ipfs/go-ipfs/issues/8769").
 						Equals("none"),
-				).Body(
-				IsCar().
-					HasRoot(fixture.MustGetCid("subdir", "ascii.txt")).
-					HasBlock(fixture.MustGetCid("subdir", "ascii.txt")).
+				).
+				Body(
+					IsCar().
+					HasRoot(fixture.MustGetCid()).
+					HasBlocks(
+						fixture.MustGetCid(),
+						fixture.MustGetCid("subdir"),
+						fixture.MustGetCid("subdir", "ascii.txt"),
+					).
 					Exactly().
 					InThatOrder(),
-			),
+				),
+		},
+		{
+			Name: "GET with ?format=car&car-scope=block params returns expected blocks",
+			Hint: `
+				car-scope=block should return a CAR file with only the root block and a
+				block for each optional path component.
+			`,
+			Request: Request().
+				Path("ipfs/{{cid}}/sub1/hello.txt", hamtFixture.MustGetCid()).
+				Query("format", "car").
+				Query("car-scope", "block"),
+			Response: Expect().
+				Status(200).
+				Body(
+					IsCar().
+					HasRoot(hamtFixture.MustGetCid()).
+					HasBlocks(
+						hamtFixture.MustGetCid(),
+						hamtFixture.MustGetCid("sub1"),
+						hamtFixture.MustGetCid("sub1", "hello.txt"),
+					).
+					Exactly().
+					InThatOrder(),
+				),
+		},
+		{
+			Name: "GET with ?format=car&car-scope=file params returns expected blocks",
+			Hint: `
+				car-scope=file should return a CAR file with all the blocks needed to 'cat'
+				a UnixFS file at the end of the specified path, or to 'ls' a UnixFS directory
+				at the end of the specified path.
+			`,
+			Request: Request().
+				Path("ipfs/{{cid}}/sub1/hello.txt", hamtFixture.MustGetCid()).
+				Query("format", "car").
+				Query("car-scope", "file"),
+			Response: Expect().
+				Status(200).
+				Body(
+					IsCar().
+					HasRoot(hamtFixture.MustGetCid()).
+					HasBlocks(
+						append(
+							[]string{
+								hamtFixture.MustGetCid(),
+								hamtFixture.MustGetCid("sub1"),
+								hamtFixture.MustGetCid("sub1", "hello.txt"),
+							},
+							hamtFixture.MustGetChildrenCids("sub1", "hello.txt")...,
+						)...,
+					).
+					Exactly().
+					InThatOrder(),
+				),
+		},
+		{
+			Name: "GET with ?format=car&car-scope=all params returns expected blocks",
+			Hint: `
+				car-scope=all should return a CAR file with the entire contiguous DAG
+				that begins at the end of the path query, after blocks required to verify path segments.
+			`,
+			Request: Request().
+				Path("ipfs/{{cid}}", hamtFixture.MustGetCid()).
+				Query("format", "car").
+				Query("car-scope", "all"),
+			Response: Expect().
+				Status(200).
+				Body(
+					IsCar().
+					HasRoot(hamtFixture.MustGetCid()).
+					HasBlocks(
+						append(
+							[]string{
+								hamtFixture.MustGetCid(),
+								hamtFixture.MustGetCid("sub1"),
+								hamtFixture.MustGetCid("sub1", "hello.txt"),
+								hamtFixture.MustGetCid("sub2"),
+								hamtFixture.MustGetCid("sub2", "hello.txt"),
+							},
+							append(
+								hamtFixture.MustGetChildrenCids("sub1", "hello.txt"),
+								hamtFixture.MustGetChildrenCids("sub2", "hello.txt")...
+							)...
+						)...,
+					).
+					Exactly().
+					InThatOrder(),
+				),
 		},
 	}
 
