9cf9d01b19423a04cf25250eabc8259e79046bfe
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-05-16 12:29:49 -0700

    test: ensure basic check_has works with string arrays properly

---
+++ tooling/check/check_has_test.go
@@ -0,0 +1,20 @@
+package check
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckHas(t *testing.T) {
+	// Create a Check instance
+	check := Has("value1", "value2", "value3")
+
+	// Test data
+	testData := []string{"value1", "value2", "value3"}
+
+	// Call .Check on the instance
+	checkOutput := check.Check(testData)
+
+	assert.True(t, checkOutput.Success, checkOutput.Reason)
+}
